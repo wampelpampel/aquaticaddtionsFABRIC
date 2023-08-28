4f29b50eb9e65cb29a94fffcf84ab279b4f18885
wampelpampel <theodor.schroeder@online.de>
2023-08-28 13:09:28 +0200

    Initial commit

---
+++ src/main/java/net/wampel/aq/AQ.java
@@ -0,0 +1,16 @@
+package net.wampel.aq;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AQ implements ModInitializer {
+	public static final String MOD_ID = "aq";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
